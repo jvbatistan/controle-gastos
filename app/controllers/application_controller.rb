1234fcd32ed65aaeb01704401e3d948c5dee2ddb
João Vitor <vitor.nascimento@sepog.fortaleza.ce.gov.br>
2026-02-09 04:38:04 -0300

    fix: guard nav counts for unauthenticated users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :set_nav_counts
+  before_action :set_nav_counts, if: :user_signed_in?
 
   def set_nav_counts
     @pending_suggestions_count = current_user.classification_suggestions.pending.count
